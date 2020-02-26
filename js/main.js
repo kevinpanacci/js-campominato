83400ca06d2786f9d3cc50b4d0b6109e9d1715e2
Kevin <kevinpanacci@hotmail.it>
2020-02-26 12:22:33 +0100

    Terminato stamattina

--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
-// 1 Il pc crea 16 numeri tra 1 e 100
-// 2 Il pc chiede all'utente un numero da 1 a 100
-// 3 se il numero è presente la partita termina
-// 4 se non è presente, ne chiede un altro fino a che non se ne inserisce uno presente
-// 5 al termine la funzione riporta il punteggio ( cioè quante volte è stato inserito un numero accettabile)
-
-var listaNumeri = [];
+var mine = [];
 var mioNumero = parseInt(prompt('Inserisci un numero da 1 a 100'));
-var numeriUtilizzati = [];                            // da completare
-var counter = 0; //contatore init
+var numeriUtilizzati = [];
+var counter = 0;
 
 for (var i = 0; i < 16; i++) {
-    listaNumeri.push(Math.floor(Math.random() * 100 + 1));               // Genera numero random da 1 a 100 potevo utilizzare f. generaRandomMinMax
+    mine.push(Math.floor(Math.random() * 100 + 1));
 }
-console.log(listaNumeri);
 
-checkList(mioNumero, listaNumeri, counter);                          // su questa chiamata va aggiunto il controllo dei numeri già utilizzati dall'utente
-function checkList (mioNumero, myList, counter) {
-    counter = counter + 1;                                         // contatore tentativi
+console.log('Le mine sono: ' + mine);
+
+if (numeriUtilizzati.includes(mioNumero)) {
+    mioNumero = parseInt(prompt('Inserisci un numero da 1 a 100, hai già inserito questo numero'));
+} else {
+    checkList (mioNumero, mine);
+}
+
+function checkList (mioNumero, myList) {
+
     if (myList.includes(mioNumero)) {
-        console.log('hai perso, il numero è contenuto nella lista. Sei riuscito però ad indovinare: ' + counter +' numeri non presenti!');
+        console.log('hai perso, il numero è contenuto nella lista. Sei riuscito però ad indovinare: ' + numeriUtilizzati.length +' numeri non presenti!');
         return; //Se il numero è contenuto fine!
     } else {
         mioNumero = parseInt(prompt('Inserisci un numero da 1 a 100'));
-        return checkList(mioNumero, listaNumeri, counter);      // se il numero non è contenuto richiama la funzione aggiornando il contatore dei tentativi
+        numeriUtilizzati.push(mioNumero);                        // DA COMPLETARE
+        return checkList(mioNumero, mine);
     }
 }
+
+console.log('I numeri vincenti utilizzati sono: ' + numeriUtilizzati);
+
+// aggiungere controlli sugli errori
